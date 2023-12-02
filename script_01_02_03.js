a3ea9b0e0196c14794e2c40fde24b811aff60ed9
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2023-12-02 18:28:28 +0600

    first commit

---
+++ script_01_02_03.js
@@ -0,0 +1,7 @@
+// DOM = Document Object Model
+// CRUD = Create Read Update Delete
+const header = document.getElementById("header");
+
+header.textContent = 'To Do textContent';
+header.innerText = 'To Do innerText';
+header.innerHTML = "To Do innerHTML";
