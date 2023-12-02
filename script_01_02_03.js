579d37c11700db897977e57daf701a9bba70a454
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2023-12-03 01:40:21 +0600

    finished DOM

--- script_01_02_03.js
+++ script_01_02_03.js
@@ -2,6 +2,11 @@
 // CRUD = Create Read Update Delete
 const header = document.getElementById("header");
 
-header.textContent = 'To Do textContent';
-header.innerText = 'To Do innerText';
-header.innerHTML = "To Do innerHTML";
+//header.textContent = 'To Do textContent';
+//header.innerText = 'To Do innerText';
+//header.innerHTML = "To Do innerHTML";
+
+//// document.getElementsByClassName("");
+//// document.getElementsByTagName("");
+//// document.querySelector("");
+document.querySelector('#items:last-child').style.color = 'red';
---
+++ script_04_06.js
@@ -0,0 +1,3 @@
+const parent = document.querySelector('#items');
+const children = parent.children;
+console.log(children);
